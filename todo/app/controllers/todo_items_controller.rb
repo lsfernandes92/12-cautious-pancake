b5145373b7ca8d02d77e1a7684ac793729132e69
lsfernandes92 <lsfernandes92@gmail.com>
2018-04-01 21:04:50 -0300

    Mark as completed ability for the todo items

--- todo/app/controllers/todo_items_controller.rb
+++ todo/app/controllers/todo_items_controller.rb
@@ -1,23 +1,38 @@
 class TodoItemsController < ApplicationController
 
   before_action :set_todo_list
+  before_action :set_todo_item, except: [:create]
 
   def create
-    @todo_item = @todo_list.todo_items.create(todo_list_params)
+    @todo_item = @todo_list.todo_items.create(todo_item_params)
     redirect_to @todo_list
+  end
+
+  def destroy
+    if @todo_item.destroy
+      flash[:notice] = "Todo item was deleted!"
+    else
+      flash[:notice] = "Todo item could not be deleted."
+    end
+    redirect_to @todo_list
+  end
+
+  def complete
+    @todo_item.update_attribute(:completed_at, Time.now)
+    redirect_to @todo_list, notice: "Todo item marked as completed!"
   end
 
   private
 
-  def renderiza(view)
-    render view
-  end
+    def set_todo_item
+      @todo_item = @todo_list.todo_items.find(params[:id])
+    end
 
-  def set_todo_list
-    @todo_list = TodoList.find(params[:todo_list_id])
-  end
+    def set_todo_list
+      @todo_list = TodoList.find(params[:todo_list_id])
+    end
 
-  def todo_list_params
-    params.require(:todo_item).permit(:content)
-  end
+    def todo_item_params
+      params.require(:todo_item).permit(:content)
+    end
 end
--- todo/config/routes.rb
+++ todo/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :todo_lists do
-    resources :todo_items
+    resources :todo_items do
+      patch :complete, on: :member
+    end
   end
 
   root "todo_lists#index"
